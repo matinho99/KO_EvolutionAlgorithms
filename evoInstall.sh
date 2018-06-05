82fedfae4636f0e8c2fb03f3a7c3b5837e2ccf8f
matinho99 <35080950+matinho99@users.noreply.github.com>
2018-06-05 22:06:39 +0200

    fltk removed

--- evoInstall.sh
+++ evoInstall.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ "$1" = "get-fltk" ] || [ "$2" = "get-fltk" ] || [ "$3" = "get-fltk" ]; then
-	sudo apt-get install libfltk1.3-dev
-fi
 if [ "$1" = "get-boost-test" ] || [ "$2" = "get-boost-test" ] || [ "$3" = "get-boost-test" ]; then
 	sudo apt-get install libboost-test-dev
 fi
